b6454f9e23da61fd3d57f18cffc155bfaaab8776
sreerag <sreerag@gmail.com>
2024-05-06 17:46:04 +0000

    Frontend WebServer Created

--- main.tf
+++ main.tf
@@ -61,4 +61,19 @@ resource "aws_security_group" "frontend_access" {
 
 
 
+resource "aws_instance" "frontend" {
+
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.auth_key.key_name
+  vpc_security_group_ids = [aws_security_group.frontend_access.id]
+  user_data              = file("setup.sh")
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-frontend"
+    Project = "${var.project_name}"
+    Env     = "${var.project_env}"
+  }
+}
+
+
 
